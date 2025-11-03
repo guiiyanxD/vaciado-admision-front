31b41dcbf8ece77d8c2ee976800d4b06ab24c4f7
Williams Alvarez Zabala <williamswaz18@gmail.com>
2025-11-03 00:51:32 -0400

    updating main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import API_BASE_URL from 'config/api'
+import API_BASE_URL from './config/api'
 
 createApp(App).use(router).use(router).use(API_BASE_URL).mount('#app')
